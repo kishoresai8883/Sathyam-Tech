f451d0e9cf38ff84a4b4819e08032c096dd622f4
Kishore Sai <kishore.sai207@gmail.com>
2025-11-18 20:00:30 +0530

    vite file updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Sathyam-Tech/",
+  server: {
+    allowedHosts: ['sathyam-tech.onrender.com']
+  },
   plugins: [
     react(),
   ],
